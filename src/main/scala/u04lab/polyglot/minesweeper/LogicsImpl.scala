248aebda332ebae63e69822956a85acf4c0ad760
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-03-30 20:34:46 +0200

    Refactor

--- src/main/scala/u04lab/polyglot/minesweeper/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/minesweeper/LogicsImpl.scala
@@ -7,28 +7,31 @@ import scala.collection.mutable
 case class Cell(position: Position, mine: Boolean, revealed: Boolean = false, flagged: Boolean = false):
   def reveal: Cell = this.copy(mine = mine, revealed = true)
   def flag: Cell = this.copy(mine = mine, revealed = revealed, flagged = !flagged)
+  def isNeighbourOf(other: Cell): Boolean =
+    !(position == other.position) && Math.abs(position.x - other.position.x) <= 1 && Math.abs(position.y - other.position.y) <= 1
 
 object Cell:
   def mine(position: Position): Cell = Cell(position, mine = true)
   def empty(position: Position): Cell = Cell(position, mine = false)
 
 case class Grid(size: Int, private val cells: mutable.Set[Cell]):
-  private def areNeighbours(cell: Cell, other: Cell): Boolean =
-    !(cell.position == other.position) && Math.abs(cell.position.x - other.position.x) <= 1 && Math.abs(cell.position.y - other.position.y) <= 1
-  def getCellAt(position: Position): Option[Cell] =
+  def cellAt(position: Position): Option[Cell] =
     cells.find(_.position == position)
-  def getNeighbours(position: Position): Set[Cell] =
-    cells.filter(areNeighbours(_, getCellAt(position).get)).toSet
+  def neighboursOf(position: Position): Set[Cell] =
+    cellAt(position).map(c => cells.filter(_ isNeighbourOf c).toSet).getOrElse(Set.empty)
   def areAllEmptyCellsRevealed: Boolean =
     !cells.exists(c => !c.mine && !c.revealed)
   def hasMineBeenRevealed: Boolean =
     cells.exists(c => c.mine && c.revealed)
+  private def replace(cell: Cell, newCell: Cell): Unit =
+    cells.remove(cell)
+    cells.add(newCell)
   def revealCellAt(position: Position): Unit =
-    getCellAt(position).foreach(c => cells.remove(c) && cells.add(c.reveal))
+    cellAt(position).foreach(c => replace(c, c.reveal))
   def flagCellAt(position: Position): Unit =
-    getCellAt(position).foreach(c => cells.remove(c) && cells.add(c.flag))
+    cellAt(position).foreach(c => replace(c, c.flag))
   def getMinesAround(position: Position): Int =
-    getNeighbours(position).count(_.mine)
+    neighboursOf(position).count(_.mine)
 
 object Grid:
   private def emptyField(size: Int): mutable.Set[Cell] =
@@ -45,21 +48,20 @@ object Grid:
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
   private val grid: Grid = Grid(size, mines)
   override def reveal(position: Position): Unit =
-    grid.getCellAt(position) match
+    grid.cellAt(position) match
       case Some(cell) if !cell.mine && !cell.revealed =>
         grid.revealCellAt(position)
         if grid.getMinesAround(position) == 0 then
-          grid.getNeighbours(position).filter(!_.mine).filter(!_.revealed).foreach(c => reveal(c.position))
+          grid.neighboursOf(position).foreach(c => reveal(c.position))
       case _ => ()
-
   override def toggleFlag(position: Position): Unit =
-    grid.flagCellAt(position)
+    grid flagCellAt position
   override def hasWon: Boolean =
     grid.areAllEmptyCellsRevealed
   override def hasLost: Boolean =
     grid.hasMineBeenRevealed
   override def getCellStatus(position: Position): CellStatus =
-    grid.getCellAt(position)
+    grid.cellAt(position)
       .orElse(Some(Cell.empty(position)))
-      .map(c => CellStatus(c.revealed, c.flagged, c.mine, grid.getMinesAround(c.position)))
+      .map(c => CellStatus(c.revealed, c.flagged, c.mine, grid getMinesAround c.position))
       .get
